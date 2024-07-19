56681288fb4304a44eff0d2629ad78bc97cdb770
ElectRICdll <2636422098@qq.com>
2024-07-19 10:42:55 +0800

    feat(webui): add injections

--- packages/im/src/index.ts
+++ packages/im/src/index.ts
@@ -1,23 +1,31 @@
-import { Context, Schema } from '@satorijs/core'
+import { Context, Dict, Schema } from '@satorijs/core'
+import {} from '@satorijs/plugin-server'
 import {} from '@cordisjs/plugin-webui'
+import {} from '@satorijs/plugin-im'
+
+// export interface Data {
+//   serverUrl: string
+//   invitations: Dict<Array<Invitation>>
+// }
 
 export const name = 'im'
 
-export const inject = {
-  required: ['webui'],
-}
+export const inject = ['webui', 'satori', 'satori.server', 'im.data', 'im.login', 'im.event']
 
 export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  ctx.webui.addEntry({
-    base: import.meta.url,
-    dev: '../client/index.ts',
-    prod: [
-      '../dist/index.js',
-      '../dist/style.css',
-    ],
-  })
+  const entry = ctx.webui.addEntry(
+    {
+      base: import.meta.url,
+      dev: '../client/index.ts',
+      prod: ['../dist/index.js', '../dist/style.css'],
+    },
+    () => ({
+      // serverUrl: ctx.satori.server.url
+      // invitations: ctx['im.invitation'].invitations
+    })
+  )
 }
